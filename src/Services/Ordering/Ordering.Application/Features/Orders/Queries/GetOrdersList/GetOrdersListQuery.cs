1897b28f7d998b6022ff9f5a8c4254c8f45b1bdd
Rafic Houssari <rafic.houssari@cmeoffshore.com>
2022-05-28 16:07:02 +0300

    Add Application layer features/queries folder

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery: IRequest<List<OrdersVm>>
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public GetOrdersListQuery(String username)
+        {
+            this.Username = username;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrdersVm.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class OrdersVm
+    {
+        public string Username { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+
+        //billing address
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        //payment
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
+    }
+}
