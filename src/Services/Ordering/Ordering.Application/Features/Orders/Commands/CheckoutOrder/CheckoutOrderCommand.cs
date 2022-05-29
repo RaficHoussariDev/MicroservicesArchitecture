8b6112b52f28e09a52392e8ff6b6a5883daf6cf5
Rafic Houssari <rafic.houssari@cmeoffshore.com>
2022-05-29 13:33:40 +0300

    Checkout order command implementation

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommand: IRequest<int>
+    {
+        public string Username { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+
+        //billing address
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        //payment
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
+    }
+}
